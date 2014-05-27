fe46cb097c64a66c26a6d16003a9754ee5df2260
heleyna <srdph.angel@gmail.com>
2014-05-27 11:45:07 +0800

    added sftp config and modified field displays on indie event content type

--- profiles/drupalexp_evolve/themes/evolve/templates/node/node--indieevent.tpl.php
+++ profiles/drupalexp_evolve/themes/evolve/templates/node/node--indieevent.tpl.php
@@ -12,7 +12,12 @@ dsm($content);
     <div class="portfolio-image col-md-5">
       <span class="brd-headling"></span>
       <div class="clearfix"></div>
-	  <p><?php print render($content['field_event_date']); ?></p>
+	    <p><?php print render($content['field_event_date']); ?></p>
+      <p><?php print render($content['field_price']); ?></p>
+      <p><?php print render($content['field_music']); ?></p>
+      <p><?php print render($content['field_dj']); ?></p>
+      <p><?php print render($content['field_event_organizer']); ?></p>
+      <p><?php print render($content['field_venue']); ?></p>
       <p><?php print render($content['body']); ?></p>
       <h3 class="headline"><?php print t('Project Details');?></h3>
       <span class="brd-headling"></span>
