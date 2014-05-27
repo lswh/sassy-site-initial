1e9ebfc972a8331aa06b2a97bf10dbd4fb6a0eb2
heleyna <srdph.angel@gmail.com>
2014-05-27 17:05:53 +0800

    icons added

--- profiles/drupalexp_evolve/themes/evolve/templates/node/node--indieevent.tpl.php
+++ profiles/drupalexp_evolve/themes/evolve/templates/node/node--indieevent.tpl.php
@@ -36,21 +36,28 @@
 
       <span class="brd-headling"></span>
 
-      <div class="clearfix"></div>
+      <div  class="clearfix"></div>
 
-	    <p><i class="fa fa-clock-o"></i><?php print render($content['field_event_date']); ?></p>
+      <p><i class="fa fa-clock-o" style="text-align:left;float:left;></i><?php print render($content['field_event_date']); ?></p>
+      <hr style="clear:both;"/>
 
-      <p><?php print render($content['field_price']); ?></p>
+      <i class="fa fa-star" style="text-align:left;float:left;></i><p style="text-align:right;float:right;"><?php print render($content['field_price']); ?></p>
+      <hr>
 
-      <p><?php print render($content['field_promotion']); ?></p>
+      <p style="text-align:right;float:right;"><i class="fa fa-glass" style="text-align:left;float:left;></i><?php print render($content['field_promotion']); ?></p>
+      <hr>
 
-      <p><?php print render($content['field_music']); ?></p>
+      <p style="text-align:right;float:right;"><i class="fa fa-music" style="text-align:left;float:left;></i><?php print render($content['field_music']); ?></p>
+      <hr>
 
-      <p><?php print render($content['field_dj']); ?></p>
+      <p style="text-align:right;float:right;"><i class="fa fa-headphones" style="text-align:left;float:left;></i><?php print render($content['field_dj']); ?></p>
+      <hr>
 
-      <p><?php print render($content['field_event_organizer']); ?></p>
+      <p style="text-align:right;float:right;"><i class="fa fa-play-circle-o" style="text-align:left;float:left;></i><?php print render($content['field_event_organizer']); ?></p>
+      <hr>
 
-      <p><?php print render($content['field_venue']); ?></p>
+      <p style="text-align:right;float:right;"><i class="fa fa-map-marker" style="text-align:left;float:left;></i><?php print render($content['field_venue']); ?></p>
+      <hr>
 
      <p><?php print render($content['body']); ?></p>
 
