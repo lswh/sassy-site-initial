f0379ca7cc0321a281c735e6127039f4b85f7367
root <root@vps64542.ovh.net>
2014-05-27 05:22:07 -0400

    removed floats

--- profiles/drupalexp_evolve/themes/evolve/templates/node/node--indieevent.tpl.php
+++ profiles/drupalexp_evolve/themes/evolve/templates/node/node--indieevent.tpl.php
@@ -38,26 +38,26 @@
 
       <div  class="clearfix"></div>
 
-      <p><i class="fa fa-clock-o" style="text-align:left;float:left;></i><?php print render($content['field_event_date']); ?></p>
-      <hr style="clear:both;"/>
+      <p><i class="fa fa-clock-o"><?php print render($content['field_event_date']); ?></p></i>
+      <hr />
 
-      <i class="fa fa-star" style="text-align:left;float:left;></i><p style="text-align:right;float:right;"><?php print render($content['field_price']); ?></p>
-      <hr>
+      <i class="fa fa-star"></i><p style="text-align:right;float:right;"><?php print render($content['field_price']); ?></p>
+      <hr />
 
-      <p style="text-align:right;float:right;"><i class="fa fa-glass" style="text-align:left;float:left;></i><?php print render($content['field_promotion']); ?></p>
-      <hr>
+      <p><i class="fa fa-glass"></i><?php print render($content['field_promotion']); ?></p>
+      <hr />
 
-      <p style="text-align:right;float:right;"><i class="fa fa-music" style="text-align:left;float:left;></i><?php print render($content['field_music']); ?></p>
-      <hr>
+      <p><i class="fa fa-music"></i><?php print render($content['field_music']); ?></p>
+      <hr />
 
-      <p style="text-align:right;float:right;"><i class="fa fa-headphones" style="text-align:left;float:left;></i><?php print render($content['field_dj']); ?></p>
-      <hr>
+      <p><i class="fa fa-headphones"></i><?php print render($content['field_dj']); ?></p>
+      <hr />
 
-      <p style="text-align:right;float:right;"><i class="fa fa-play-circle-o" style="text-align:left;float:left;></i><?php print render($content['field_event_organizer']); ?></p>
-      <hr>
+      <p><i class="fa fa-play-circle-o"></i><?php print render($content['field_event_organizer']); ?></p>
+      <hr />
 
-      <p style="text-align:right;float:right;"><i class="fa fa-map-marker" style="text-align:left;float:left;></i><?php print render($content['field_venue']); ?></p>
-      <hr>
+      <p><i class="fa fa-map-marker"></i><?php print render($content['field_venue']); ?></p>
+      <hr />
 
      <p><?php print render($content['body']); ?></p>
 
