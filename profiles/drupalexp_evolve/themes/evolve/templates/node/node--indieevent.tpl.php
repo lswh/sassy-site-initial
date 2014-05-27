86b99d264ef0aca96da9f2b2214833a26abf80fb
root <root@vps64542.ovh.net>
2014-05-27 03:01:38 -0400

    julien change new tpl node

--- profiles/drupalexp_evolve/themes/evolve/templates/node/node--indieevent.tpl.php
+++ profiles/drupalexp_evolve/themes/evolve/templates/node/node--indieevent.tpl.php
@@ -4,17 +4,27 @@
 // We hide the comments and links now so that we can render them later.
     hide($content['comments']);
     hide($content['links']);
-dsm($content);    
+    dsm($content);    
     ?>
     <div class="portfolio-image col-md-7">
       <?php  print render($content['field_add_image']); ?>
+        <div style="text-align:center;">
+        <div onclick="jQuery('#popUp_loginNotice').show();" class=" btn btn-small btn-primary btn-block">
+        I'm Attending
+        </div>
+        </div>
     </div>
     <div class="portfolio-image col-md-5">
       <span class="brd-headling"></span>
       <div class="clearfix"></div>
-	  <p><?php print render($content['field_event_date']); ?></p>
+	    <p><?php print render($content['field_event_date']); ?></p>
+      <p><?php print render($content['field_price']); ?></p>
+      <p><?php print render($content['field_promotion']); ?></p>
+      <p><?php print render($content['field_music']); ?></p>
+      <p><?php print render($content['field_dj']); ?></p>
+      <p><?php print render($content['field_event_organizer']); ?></p>
+      <p><?php print render($content['field_venue']); ?></p>
       <p><?php print render($content['body']); ?></p>
-      <h3 class="headline"><?php print t('Project Details');?></h3>
       <span class="brd-headling"></span>
       <div class="clearfix"></div>
       <?php print render($content['field_portfolio_categories']);?>
