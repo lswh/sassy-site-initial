9c75137b9d6f5129258e79ed949c389adc576360
heleyna <srdph.angel@gmail.com>
2014-05-29 12:29:10 +0800

    node form edits on local

--- profiles/drupalexp_evolve/themes/evolve/templates/node/indieevent-node-form.tpl.php
+++ profiles/drupalexp_evolve/themes/evolve/templates/node/indieevent-node-form.tpl.php
@@ -1,16 +1,16 @@
 <?php
 dpm($form);
 ?>
-    <div class="portfolio-image col-md-6">
+    <div class="portfolio-image col-md-4">
       <p><?php print render($form['title']); ?></p>
       <p><?php print render($form['body']); ?></p>
-      <p><?php  print render($form['field_add_image']); ?></p>
+
       <p><?php  print render($form['field_event_type']); ?></p>
       <p><?php  print render($form['field_tags']); ?></p>
       <p><?php  print render($form['field_is_this_a_private_event_']); ?></p>
       <p><?php  print render($form['field_invitation']); ?></p>
     </div>
-    <div class="portfolio-image col-md-6">
+    <div class="portfolio-image col-md-4">
       <span class="brd-headling"></span>
       <div  class="clearfix"></div>
       <i class="fa fa-clock-o"></i><p><?php print render($form['field_event_date']); ?></p>
@@ -25,16 +25,19 @@ dpm($form);
       <hr />
       <p><i class="fa fa-play-circle-o"></i><?php print render($form['field_event_organizer']); ?></p>
       <hr />
-      <p><i class="fa fa-map-marker"></i><?php print render($form['field_venue']); ?></p>
-      <p><?php print render($form['field_venue_name']); ?></p>
-      <p><?php print render($form['field_new_venue_description']); ?></p>
       <span class="brd-headling"></span>
       <div class="clearfix"></div>
     </div>
+    <div class="portfolio-image col-md-4">
+      <p><?php  print render($form['field_add_image']); ?></p>
+      <p><i class="fa fa-map-marker"></i><?php print render($form['field_venue']); ?></p>
+      <p><?php print render($form['field_venue_name']); ?></p>
+      <p><?php print render($form['field_new_venue_description']); ?></p>
+    </div>
     <div  class="clearfix"></div>
     <div class="portfolio-image col-md-11">
-    <center><p><?php  print render($form['field_map']); ?></p>
-    <?php print drupal_render_children($form);?></center>
+    <center><p><?php  print render($form['field_map']); ?></p></center>
+    <?php print drupal_render_children($form);?>
     </div>
 
 
