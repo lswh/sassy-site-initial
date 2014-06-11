fdcd8c0e6030450fb62744635924d0030812d676
heleynaovh <srdph.angel@gmail.com>
2014-06-10 21:58:10 -0400

    renamed mag view file

--- profiles/drupalexp_evolve/themes/evolve/templates/views/views-view-dexp-portfolio--blog_edited_hln--page_monthly_timeline.tpl.php
+++ profiles/drupalexp_evolve/themes/evolve/templates/views/views-view-dexp-portfolio--blog_edited_hln--page_monthly_timeline.tpl.php
@@ -1,3 +1,17 @@
+<?php //if($options['unformatted_blog_filter']):?>
+  <?php if(isset($categories)):?>
+  <div class="unformatted-filters">
+		<ul id="<?php print $filter_id;?>" class="unformatted-filter clearfix" data-option-key="filter">
+			<li><a class="active" href="#" data-filter="*"><?php print t('Show All')?></a></li>
+			<?php foreach($categories as $key => $c): ?>
+				<li>
+					<a href="#" data-filter="<?php echo $key; ?>"><?php echo $c; ?></a>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	</div>
+  <?php endif;?>
+//<?php //endif;?>
 <div class="custompadding2" data-padding="0">
 <div id ="tline-content">
   <div class="tline-topdate"><?php print $title;?></div>
